ad716aa299d12b8c6bcba2164309290801e08446
MasterHacker10 <141876965+MasterHacker10@users.noreply.github.com>
2024-05-29 17:50:15 -0400

    good game

---
+++ Scavenger Hunt/Assets/Scripts/Score.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    [Header("Default Score")]
+    public int score = 0;
+    [Header("Text Object for Displaying Score")]
+    public Text scoreText;
+
+    public void Start()
+    {
+        scoreText.text = "Ṡ̷͉̹̬̱͙̦͈͑̀̿͛̄̔̔̽̐͠͠c̵͍̈́̿̀̈̃͒̅̓̀̅̌o̷̦̻̻͆̆̒̇̇̂̔́̈́̾̊̿͝͠ȓ̵̡̡̛̝̫̟̘͙͉̞̳͎̎͗̇͊̓̉͗͌̆͠e̶͓̜̻̮̞̖͕̬̩̮̓̿̓͂͂͌́̒̃͝:̶̧̧̰̻̩̭̫̯̱̻͎̜̯̒̏͛̀͒̚͘͝: " + score.ToString();
+    }
+
+
+    public void AddScore(int points)
+    {
+        score = score + points;
+        scoreText.text = "Ṡ̷͉̹̬̱͙̦͈͑̀̿͛̄̔̔̽̐͠͠c̵͍̈́̿̀̈̃͒̅̓̀̅̌o̷̦̻̻͆̆̒̇̇̂̔́̈́̾̊̿͝͠ȓ̵̡̡̛̝̫̟̘͙͉̞̳͎̎͗̇͊̓̉͗͌̆͠e̶͓̜̻̮̞̖͕̬̩̮̓̿̓͂͂͌́̒̃͝:̶̧̧̰̻̩̭̫̯̱̻͎̜̯̒̏͛̀͒̚͘͝: " + score.ToString();
+    }
+}
