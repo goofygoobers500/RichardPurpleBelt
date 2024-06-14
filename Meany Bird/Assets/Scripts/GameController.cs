8af77d2a78c83f276e9e5911272ede6aa3f60130
MasterHacker10 <>
2024-06-14 15:31:05 -0400

    Sensei: Make sure you push changes!!!

---
+++ Meany Bird/Assets/Scripts/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject gameOverCanvas;
+
+
+    public GameObject scoreCanvas;
+
+
+    public GameObject spawner;
+    void Start()
+    {
+        Time.timeScale = 1;
+
+        scoreCanvas.SetActive(true);
+
+        gameOverCanvas.SetActive(false);
+        spawner.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void GameOver()
+    {
+        gameOverCanvas.SetActive(true);
+        spawner.SetActive(false);
+        Time.timeScale = 0;
+
+    }
+}
+
+
---
+++ Meany Bird/Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [Header("Spikes Object for controling the game")]
+    public GameObject stalagmite_1;
+
+    [Header("Default Height")]
+    public float height;
+    void Start()
+    {
+        InvokeRepeating("InstantiateObjects", 1f, 4f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(5, Random.Range(-height, height), 0);
+    }
+
+    void InstantiateObjects()
+    {
+        Instantiate(stalagmite_1, transform.position, transform.rotation);
+
+    }
+}
