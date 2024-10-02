a8404b6c7f1b130117f58a212319cf700c311f01
MasterHacker10 <164410221+Hugokh36@users.noreply.github.com>
2024-10-02 17:59:17 -0400

    asd

---
+++ CT - SulkySLiems/Assets/Scripts/MouseManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour
+{
+    [Header("Mouse Info")]
+    public Vector3 clickStartLocation;
+
+    [Header("Physics")]
+    public Vector3 launchVector;
+
+    public float launchForce;
+
+    [Header("Slime")]
+    public Transform slimeTransform;
+    public Rigidbody slimeRigidbody;
+
+    public Vector3 originalSlimePosition;
+    public Quaternion SlimeRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalSlimePosition = slimeTransform.position;
+        SlimeRotation = slimeTransform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            clickStartLocation = Input.mousePosition;
+            //slimeTransform.position = originalSlimePosition - launchVector;
+
+        }
+        if (Input.GetMouseButton(0))
+        {
+            slimeRigidbody.isKinematic = true;
+            Vector3 mouseDifference = clickStartLocation - Input.mousePosition;
+            launchVector = new Vector3(
+                mouseDifference.y * 1f,
+                mouseDifference.y * 1.2f,
+                mouseDifference.x * -1.5f
+            );
+            slimeTransform.position = originalSlimePosition - launchVector / 1000; //2000
+            launchVector.Normalize();
+
+            //Vector2 launchForce = new Vector3(50000, 1000, 50000);
+            //slimeRigidbody.AddForce(mouseDifference * 10);
+        }
+        /*if (Input.GetMouseButtonUp(0))
+        {
+            slimeRigidbody.isKinematic = false;
+            Vector2 launchForce = new Vector3(50000, 1000, 50000);
+            slimeRigidbody.AddForce(launchForce);
+        }*/
+        if (Input.GetMouseButtonUp(0))
+        {
+            slimeRigidbody.isKinematic = false;
+            //slimeRigidbody.AddForce(launchVector * launchForce);
+           slimeRigidbody.AddForce(launchVector * launchForce, ForceMode.Impulse);
+            Debug.Log(launchVector * launchForce);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            slimeTransform.position = originalSlimePosition;
+            slimeTransform.rotation = SlimeRotation;
+        }
+    }
+}
---
+++ New Unity Project (1)/Assets/GameController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+    public GameObject currentCube;
+    public GameObject lastCube;
+    public Text text;
+    public int Level;
+    public bool Done;
+  
+
+    IEnumerator X()
+    {
+
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    void Start()
+    {
+
+        Newblock();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Done)
+        {
+
+            return;
+        }
+        var time = Mathf.Abs(Time.realtimeSinceStartup % 3f - 1f);
+
+        var pos1 = lastCube.transform.position + Vector3.up * 10f;
+        var pos2 = pos1 + ((Level % 2 == 0) ? Vector3.left : Vector3.forward) * 120;
+        if (Level % 2 == 0)
+        {
+            currentCube.transform.position = Vector3.Lerp(pos2, pos1, time);
+
+        }
+        else
+        {
+
+
+            currentCube.transform.position = Vector3.Lerp(pos2, pos1, time);
+
+
+            {
+                if (Input.GetMouseButtonDown(0))
+
+                {
+
+                    Newblock();
+                }
+
+
+            }
+        }
+    }
+    void Newblock()
+    {
+
+        lastCube = currentCube;
+
+        currentCube = Instantiate(lastCube);
+        currentCube.name = Level + "";
+        currentCube.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.HSVToRGB((Level / 100f) % 1f, 1f, 1f));
+        Camera.main.transform.position = currentCube.transform.position + new Vector3(100, 100, 100);
+        Camera.main.transform.LookAt(currentCube.transform.position);
+
+        if (lastCube != null)
+
+            if (currentCube.transform.localScale.x < +0f ||
+                    currentCube.transform.localScale.z <= 0f)
+            {
+                Done = true;
+                text.gameObject.SetActive(true);
+                text.text = "Final Score:" + Level;
+                StartCoroutine(X());
+                return;
+            }
+        currentCube.transform.position = new Vector3(Mathf.Round(currentCube.transform.position.x),
+            currentCube.transform.position.y,
+            Mathf.Round(currentCube.transform.position.z));
+        currentCube.transform.localScale = new Vector3(lastCube.transform.localScale.x - Mathf.Abs(currentCube.transform.position.x - lastCube.transform.position.x),
+                                                        lastCube.transform.localScale.y,
+                                                        lastCube.transform.localScale.z - Mathf.Abs(currentCube.transform.position.z - lastCube.transform.position.z));
+        currentCube.transform.position = Vector3.Lerp(currentCube.transform.position, lastCube.transform.position, 0.5f) + Vector3.up * 5f;
+
+    }
+
+
+}
