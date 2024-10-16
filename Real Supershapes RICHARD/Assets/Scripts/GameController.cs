9d2908ac35d3ebf05e747fc5b8b38c3a01078b9a
MasterHacker10 <164410221+Hugokh36@users.noreply.github.com>
2024-10-16 18:02:46 -0400

    good woerkl

---
+++ Real Supershapes RICHARD/Assets/Scripts/GameController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+
+   
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        InvokeRepeating("Spawn", spawnDelay, spawnTime);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public GameObject[] shapePrefabs;
+
+
+    public float spawnDelay = 2f;
+
+    public float spawnTime = 3f;
+
+
+    void Spawn()
+    {
+
+        int randomInt = Random.Range(0, shapePrefabs.Length);
+
+
+        Instantiate(shapePrefabs[randomInt], Vector3.zero, Quaternion.identity);
+    }
+
+    public void GameOver()
+    {
+
+        CancelInvoke("Spawn");
+
+        Time.timeScale = 0;
+
+        gameOverCanvas.SetActive(true);
+    }
+
+
+    public GameObject gameOverCanvas;
+
+
+    
+}
---
+++ Real Supershapes RICHARD/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+public float moveSpeed = 600f;
+    public Transform rizz;
+
+    float movement = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Time.timeScale = 1;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        movement = Input.GetAxisRaw("Horizontal");
+        Debug.Log(Time.timeScale);
+
+    }
+
+    
+
+     void FixedUpdate()
+    {
+
+        rizz.RotateAround(Vector3.zero, Vector3.forward, movement * Time.fixedDeltaTime * -moveSpeed);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject.Find("GameController").GetComponent<GameController>().GameOver();
+    }
+}
