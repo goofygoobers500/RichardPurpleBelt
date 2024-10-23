0f24e1ddded47179d58671662bd23fcbd372e170
MasterHacker10 <164410221+Hugokh36@users.noreply.github.com>
2024-10-23 18:00:09 -0400

    RID:LKFJ:LDSKJFLKSDJF:LKSJD:LFKJSD:LFJ:LSKDJF:LKSDJF:LKSJDF:LKJDF:LKDJSF:LKSDJF:SD

---
+++ Polyrun Richard/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    
+    void Start()
+    {
+        rb = transform.GetComponent<Rigidbody2D>();
+        posX = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public float jumpPower = 6f;
+    public bool isGrounded = false;
+    float posX = 0.0f;
+    Rigidbody2D rb;
+
+    private void FixedUpdate()
+    {
+        
+
+
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+                {
+
+
+            rb.AddForce(Vector3.up * (jumpPower * rb.mass * rb.gravityScale * 20.0f));
+
+
+        }
+    }
+}
