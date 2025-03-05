c937226f92def21142a5cc468c1ebe84bb5b29ac
MasterHacker10 <quinnsidell@icloud.com>
2025-03-05 18:11:16 -0500

    so close to brown belt

--- Richard Dropping Bombs pt.2/Assets/GameManager.cs
+++ Richard Dropping Bombs pt.2/Assets/GameManager.cs
@@ -21,18 +21,31 @@ public class GameManager : MonoBehaviour
     public Text scoreText;
     public int pointsWorth = 1;
     private int score;
+
+    private int bestScore = 0;
+    public TMP_Text bestScoreText;
+    private bool beatBestScore;
+
+
+    private bool smokeCleared = true;
     private void Awake()
-
-
     {
         spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        scoreText.enabled = false;
+
+        bestScoreText.enabled = false;
+        
     }
     // Start is called before the first frame update
     void Start()
     {
         spawner.active = false;
         title.SetActive(true);
+        splash.SetActive(false);
+
+        bestScore = PlayerPrefs.GetInt("BestScore");
+        bestScoreText.text = "Best Score:" + bestScore.ToString();
     }
 
 
@@ -64,19 +77,24 @@ public class GameManager : MonoBehaviour
 
         var nextBomb = GameObject.FindGameObjectsWithTag("Bomb");
 
-        foreach (GameObject bombObject in nextBomb)
-            if (bombObject.transform.position.y < (-screenBounds.y - 12))
-            {
-
-                if (gameStarted)
-                {
-
+        foreach (GameObject bombObject in nextBomb) {
+            
+            if (bombObject.transform.position.y < (-screenBounds.y - 12)) {
+                Debug.Log("sssss");
+                if (gameStarted) {
+                
                     score += pointsWorth;
+Debug.Log(score);
                     scoreText.text = "Score: " + score.ToString();
+                    Debug.Log("Score: " + score.ToString());
                 }
                 Destroy(bombObject);
             }
+        }
     }
+
+
+  
 
     void ResetGame()
     {
@@ -88,7 +106,8 @@ public class GameManager : MonoBehaviour
         scoreText.enabled = true;
         score = 0;
 
-        scoreText.text = "Score: " + score.ToString();
+        beatBestScore = false;
+        bestScoreText.enabled = true;
 
         player = Instantiate(playerPrefab, new Vector3(0, 0, 0), playerPrefab.transform.rotation);
         gameStarted = true;
@@ -102,6 +121,15 @@ public class GameManager : MonoBehaviour
         gameStarted = false;
 
         splash.SetActive(true);
+
+        Invoke("SPlashScreen", 2);
+         if(score > bestScore)
+            {
+            bestScore = score;
+            PlayerPrefs.SetInt("BestScore", bestScore);
+            beatBestScore = true;
+            bestScoreText.text = "Best Score: " + bestScore.ToString();
+        }
 
 
     }
